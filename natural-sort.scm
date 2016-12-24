ad37166670ceb543a8cbeab44136298fc2636c9e
Vasilij Schneidermann <v.schneidermann@gmail.com>
2016-12-24 22:38:03 +0100

    Add initial implementation

---
+++ natural-sort.scm
@@ -0,0 +1,81 @@
+(module natural-sort
+  (number-re natural-sort-preprocessor
+   natural-string-compare
+   natural-string<? natural-string<=?
+   natural-string=? natural-string<>?
+   natural-string>? natural-string>=?
+   natural-sort)
+
+(import chicken scheme)
+
+(use irregex data-structures)
+
+(define number-re (make-parameter '(+ num)))
+
+(define (tokenize string)
+  (irregex-fold
+   (number-re)
+   (lambda (i m acc)
+     (let ((number (string->number (irregex-match-substring m)))
+           (start (irregex-match-start-index m)))
+       (if (= start i)
+           (cons number acc)
+           ;; fetch token before number
+           (cons number (cons (substring string i start) acc)))))
+   '() string
+   (lambda (i acc)
+     (let ((end (string-length string)))
+       (if (= i end)
+           (reverse acc)
+           ;; fetch token after last number
+           (reverse (cons (substring string i end) acc)))))))
+
+(define natural-sort-preprocessor (make-parameter identity))
+
+(define (natural-string-compare string1 string2)
+  (define (clamp lower x upper)
+    (min (max lower x) upper))
+  (define (inner as bs)
+    (cond
+     ((and (null? as) (null? bs)) 0)
+     ((null? as) -1)
+     ((null? bs) 1)
+     (else
+      (let ((a (car as))
+            (b (car bs)))
+        (if (and (number? a) (number? b))
+            (cond
+             ((< a b) -1)
+             ((> a b) 1)
+             (else (inner (cdr as) (cdr bs))))
+            (let* ((a (if (number? a) (number->string a) a))
+                   (b (if (number? b) (number->string b) b))
+                   (result (string-compare3 a b)))
+              (if (zero? result)
+                  (inner (cdr as) (cdr bs))
+                  (clamp -1 result 1))))))))
+  (inner (tokenize ((natural-sort-preprocessor) string1))
+         (tokenize ((natural-sort-preprocessor) string2))))
+
+(define (natural-string<? string1 string2)
+  (if (< (natural-string-compare string1 string2) 0) #t #f))
+
+(define (natural-string<=? string1 string2)
+  (if (<= (natural-string-compare string1 string2) 0) #t #f))
+
+(define (natural-string=? string1 string2)
+  (if (zero? (natural-string-compare string1 string2)) #t #f))
+
+(define (natural-string<>? string1 string2)
+  (if (not (zero? (natural-string-compare string1 string2))) #t #f))
+
+(define (natural-string>? string1 string2)
+  (if (> (natural-string-compare string1 string2) 0) #t #f))
+
+(define (natural-string>=? string1 string2)
+  (if (>= (natural-string-compare string1 string2) 0) #t #f))
+
+(define (natural-sort strings)
+  (sort strings natural-string<?))
+
+)
---
+++ tests/run.scm
@@ -0,0 +1,52 @@
+(use natural-sort test srfi-13)
+
+(test-group "Comparison"
+  (test  0 (natural-string-compare "" ""))
+  (test -1 (natural-string-compare "" "foo"))
+  (test +1 (natural-string-compare "foo" ""))
+  (test  0 (natural-string-compare "foo" "foo"))
+  (test -1 (natural-string-compare "bar" "foo"))
+  (test +1 (natural-string-compare "foo" "bar"))
+  (test  0 (natural-string-compare "foo123" "foo123"))
+  (test -1 (natural-string-compare "foo" "foo123"))
+  (test +1 (natural-string-compare "foo123" "foo"))
+  (test -1 (natural-string-compare "foo2" "foo123"))
+  (test -1 (natural-string-compare "foo99" "foo123"))
+  (test  0 (natural-string-compare "foo123bar" "foo123bar"))
+  (test -1 (natural-string-compare "foo123bar" "foo123baz"))
+  (test +1 (natural-string-compare "foo123qux" "foo123baz")))
+
+(test-group "Predicates"
+  (test #t (natural-string<?  "foo9" "foo10"))
+  (test #t (natural-string<=? "foo9" "foo10"))
+  (test #f (natural-string=?  "foo9" "foo10"))
+  (test #t (natural-string<>? "foo9" "foo10"))
+  (test #f (natural-string>?  "foo9" "foo10"))
+  (test #f (natural-string>=? "foo9" "foo10")))
+
+(test-group "Flonums (default)"
+  (test #f (natural-string<? "1.001" "1.01"))
+  (test #f (natural-string<? "1.01" "1.1")))
+
+(test-group "Flonums (custom)"
+  (parameterize ((number-re '(: (+ num) (? (: "." (+ num))))))
+    (test #t (natural-string<? "1.001" "1.01"))
+    (test #t (natural-string<? "1.01" "1.1"))))
+
+(test-group "Sorting (default)"
+  (test '("1" "2" "3" "4" "10" "20" "30" "100")
+        (natural-sort '("1" "10" "100" "2" "20" "3" "30" "4")))
+  (test '("a" "a0" "a1" "a1a" "a1b" "a2" "a10" "a20")
+        (natural-sort '("a10" "a" "a20" "a1b" "a1a" "a2" "a0" "a1")))
+  (test '("1.2.3.4" "8.8.8.8" "127.0.0.1" "192.168.178.1")
+        (natural-sort '("127.0.0.1" "8.8.8.8" "192.168.178.1" "1.2.3.4"))))
+
+(test-group "Sorting (custom)"
+  (parameterize ((natural-sort-preprocessor string-trim))
+    (test '(" a9" "a10" " a11" "a20")
+          (natural-sort '("a10" " a9" "a20" " a11"))))
+  (parameterize ((natural-sort-preprocessor string-downcase))
+    (test '("a9" "A10" "a11" "A20")
+          (natural-sort '("A10" "a9" "A20" "a11")))))
+
+(test-exit)
